c6ddf4bc02a56006c865e0b4457fb6974dc09135
João Batista <joaobmc88@gmail.com>
2021-12-09 06:53:01 -0300

    Fim Projeto IMC

--- IMC/script.js
+++ IMC/script.js
@@ -1,1 +1,35 @@
-console.log("hello")
+
+const calculate = document.getElementById("calcular");
+
+
+function imc (){
+    const name = document.getElementById("nome").value;
+    const height = document.getElementById("altura").value;
+    const weight = document.getElementById("peso").value;
+    const result = document.getElementById("resultado");
+
+    if(name != '' && height != '' && weight != '')
+    {
+        const imcResul = (weight / (height * height)).toFixed(2);
+        let classfication = '';
+        if(imcResul < 18.5){
+            classfication = "abaixo do peso.";
+        }else if (imcResul < 25){
+            classfication = "com o peso ideal";
+        }else if (imcResul < 30){
+            classfication = "com Sobrepeso";
+        }else if (imcResul < 35) {
+            classfication = "com Obesidade Gral I";
+        }else if (imcResul < 40){
+            classfication = "com Obesidade Gral II";
+        }else if (imcResul >= 40)        {
+            classfication = "com Obesidade Gral II ou Mórbida";
+        }
+        result.textContent =`${name} seu IMC é: ${imcResul} e você está ${classfication}`;
+    }else
+    {
+        result.textContent = "Preencha todos os campos!!";
+    }
+   
+}
+calculate.addEventListener('click',imc);
